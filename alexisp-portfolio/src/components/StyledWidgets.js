9b44f079d50c847c8310adf0fa9a1d0bbcabb665
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-03-19 18:42:59 -0500

    Progress:
    I have added my projects that I want to display and now starting to work on styling.

--- alexisp-portfolio/src/components/StyledWidgets.js
+++ alexisp-portfolio/src/components/StyledWidgets.js
@@ -14,6 +14,18 @@ img {
 }
 `;
 
+//Main Context
+export const MainContext = styled.div`
+    p {
+        margin-top: 30px;
+        font-size: 16px;
+    }
+`;
+
+export const Title = styled.div`
+    font-size: 18px;
+`;
+
 //Footer
 export const Icons = styled.div`
     display: flex;
@@ -34,4 +46,70 @@ export const Icons = styled.div`
         margin-top: 20px;
         
     }
+`;
+
+export const CardOne = styled.div`
+    margin: 0 auto;
+    margin-bottom: 80px;
+    width: 80%;
+    p {
+        font-size: 18px;
+    }
+`;
+
+export const CardTwo = styled.div`
+    margin: 0 auto;
+    margin-bottom: 80px;
+    width: 80%;    
+    p {
+        font-size: 18px;
+    }
+`;
+
+export const CardThree = styled.div`
+    margin: 0 auto;
+    margin-bottom: 80px;
+    width: 80%;
+    p {
+        font-size: 18px;
+    }
+`;
+
+export const CardFour = styled.div`
+    margin: 0 auto;
+    margin-bottom: 80px;
+    width: 80%;
+    p {
+        font-size: 18px;
+    }
+`;
+
+export const CardFive = styled.div`
+    margin: 0 auto;
+    margin-bottom: 80px;
+    width: 80%;
+    p {
+        font-size: 18px;
+    }
+`;
+
+export const CardSix = styled.div`
+    margin: 0 auto;
+    margin-bottom: 80px;
+    width: 80%;
+    p {
+        font-size: 18px;
+    }
+`;
+
+//Header One
+export const HeaderOne = styled.header`
+    font-size: 35px;
+    font-family: Lato regular;
+`;
+
+//Header Two
+export const HeaderTwo = styled.header`
+    font-size: 35px;
+    font-family: Lato regular;
 `;
--- alexisp-portfolio/src/components/WelcomePage.js
+++ alexisp-portfolio/src/components/WelcomePage.js
@@ -5,98 +5,139 @@ import { SocialIcon } from 'react-social-icons';
 
 
 //Styled Components
-import { IMG, Icons } from './StyledWidgets';
+import { IMG, Icons, CardOne, CardTwo, CardThree, CardFour, CardFive, CardSix, HeaderOne, HeaderTwo, MainContext, Title } from './StyledWidgets';
 
 const WelcomePage = () => {
     
     return (
         <div className='welcome-page'>
-            <Card style={{ width: '40%', margin: '0 auto', marginBottom: '50px', marginTop: '20px', textAlign: 'center'}}>
-                <Card.Body>
-                    <Card.Title>Alexis Panyathong</Card.Title>
-                    <p>Full Stack Web Developer</p>
-                    
-                    <IMG>
-                        <img className="img-container" src={require('/Users/alexispanyathong/Documents/Lambda School/AlexisP-Portfolio/alexisp-portfolio/src/imgs/IMG_9272-WM-resize.jpg')} alt="img of Alexis." />
-                    </IMG>
-                    <Card.Text>
-                    Highly determined, motivated Full Stack Web Developer with a passion for web application development. As I built many web applications I have gained a massive amount of experience with working on multiple languages. I am seeking a new opportunity that will allow me to further expand these skills. 
-                    </Card.Text>
-                    <a href="https://www.linkedin.com/in/alexis-panyathong" target="_blank">LinkedIn</a>
-                </Card.Body>
-            </Card>
-
-            <Card className="text-center">
-                <Card.Header>Featured</Card.Header>
-                <Card.Body>
-                    <Card.Title>User-Interface</Card.Title>
-                    <Card.Text>
-                    This project was a study on HTML and CSS. I resized some images and texts to align with the website.
-                    </Card.Text>
-                    <Button variant="primary"                     href="https://github.com/AlexisPanyathong/User-Interface" target="_blank">View Here</Button>
-                </Card.Body>
-                <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card>
-            <Card className="text-center">
-                <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                        <Card.Title>GitHub-UserCard</Card.Title>
-                        <Card.Text>
-                        In this project we learned how to access the GitHub API and built a social card based on the data we got back. One of the main goals was to create a component based on the data we got back when we sent a GET request to the GitHub API. 
-                        This project consists of:
-						<li>Creating DOM components with Javascript Functions.</li>
-						<li>Utilizing 3rd party libraries (axios)</li>
-						<li>Promises, .then & .catch</li>
-						<li>HTTP GET requests</li>
-						<li>Array Methods</li>
-						<li>DOM</li>
-						<li>Element selection</li>
-						<li>Basic DOM manipulation</li>
-						<li>Events and event listeners</li>
-							
-                        </Card.Text>
-                        <Button variant="primary" href="https://github.com/AlexisPanyathong/github-usercard" target="_blank">View Here</Button>
-                    </Card.Body>
-                    <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card>
             
-            <Card className="text-center">
-                <Card.Header>Featured</Card.Header>
+                <Card style={{ width: '40%', margin: '0 auto', marginBottom: '50px', marginTop: '20px', textAlign: 'center'}}>
+                <MainContext>
                     <Card.Body>
-                        <Card.Title>Form Management</Card.Title>
-                        <Card.Text>
-                            This project demonstrates proficiency by creating an application that has a registration page and let's you register a new user. Once a user is registered, you can fetch data and render it to the DOM.
-                            This project consists of:
-                            <li>A registration form built with formik - include inputs for a username and a password.</li>
-                            <li>Form validation using Yup</li>
-                            <li>At least one class component (I would challenge you to make the form a class!)</li>
-                            <li>Tests for at least one element, one event, and one unit test for a function.</li>
-                            <li>A POST request made from the registration form to http://localhost:5000/api/register to add a user. </li>
-                            <li>A GET request made to http://localhost:5000/api/restricted/data to retrieve the data. </li>
-                            <li>Map over the array of objects you recieve and render some or all of it's information to the DOM.</li>		
-                        </Card.Text>
-                        <Button variant="primary" href="https://github.com/AlexisPanyathong/github-usercard" target="_blank">View Here</Button>
+                        <HeaderOne>Alexis Panyathong</HeaderOne>
+                        <Title>Full Stack Web Developer</Title>
+                        
+                        <IMG>
+                            <img className="img-container" src={require('/Users/alexispanyathong/Documents/Lambda School/AlexisP-Portfolio/alexisp-portfolio/src/imgs/IMG_9272-WM-resize.jpg')} alt="img of Alexis." />
+                        </IMG>
+                        <p>
+                        Highly determined, motivated Full Stack Web Developer with a passion for web application development. As I built many web applications I have gained a massive amount of experience with working on multiple languages. I am seeking a new opportunity that will allow me to further expand these skills. 
+                        </p>
+                        <a href="https://www.linkedin.com/in/alexis-panyathong" target="_blank">LinkedIn</a>
                     </Card.Body>
-                    <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card>
+                </MainContext>
+                </Card>
+            
 
-            <Card className="text-center">
-                <Card.Header>Featured</Card.Header>
+            <CardOne>
+                <Card className="text-center">
+                    <Card.Header>Featured</Card.Header>
                     <Card.Body>
-                        <Card.Title>Java SnackBar</Card.Title>
-                        <Card.Text>
-                        The main object of this project is to set up vending machines where a person buys snacks from those machines.
-                            This project consists of:
-                            <li>Use the Java Development Kit (JDK)</li>
-                            <li>Use and manipulate Java base data types</li>
-                            <li>Construct Java classes including instantiating and using Java Objects</li>
-                            <li>Use and manipulate Java base data types	</li>
-                        </Card.Text>
-                        <Button variant="primary" href="https://github.com/AlexisPanyathong/java-SnackBar/tree/alexis-panyathong-2" target="_blank">View Here</Button>
+                        <HeaderTwo>User-Interface</HeaderTwo>
+                        <p>
+                        This project was a study on HTML and CSS. I resized some images and texts to align with the website.
+                        </p>
+                        <Button variant="primary" href="https://github.com/AlexisPanyathong/User-Interface" target="_blank">View Here</Button>
                     </Card.Body>
-                    <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card>
+                    {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
+                </Card>
+            </CardOne>
 
+            <CardTwo>
+                <Card className="text-center">
+                    <Card.Header>Featured</Card.Header>
+                        <Card.Body>
+                            <HeaderTwo>WeddingGram</HeaderTwo>
+                            <p>
+                                This was my first build week at Lambda and I was a part of the UI team. Our app is called WeddingGram and this is an app to help newly engaged couples for their wedding preparations such as, finding a wedding planner, creating a timeline for their wedding, and posting their experience and questions that they may have for wedding planners. I was in charge of the How It Works, Wedding Day Timeline Table, and the Featured Wedding Planners carousel sections. I was also in charge of the About Us context and the Meet the Team tabs.
+                            </p>
+                            <Button variant="primary" href="https://github.com/buildweek-weddingportfolio/User-Interface" target="_blank">View Here</Button>
+                        </Card.Body>
+                        {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
+                </Card>
+            </CardTwo>
+
+            <CardThree>
+                <Card className="text-center">
+                    <Card.Header>Featured</Card.Header>
+                        <Card.Body>
+                            <HeaderTwo>GitHub-UserCard</HeaderTwo>
+                            <p>
+                            In this project we learned how to access the GitHub API and built a social card based on the data we got back. One of the main goals was to create a component based on the data we got back when we sent a GET request to the GitHub API.<br/> 
+                            This project consists of:
+                            <li>Creating DOM components with Javascript Functions.</li>
+                            <li>Utilizing 3rd party libraries (axios)</li>
+                            <li>Promises, .then & .catch</li>
+                            <li>HTTP GET requests</li>
+                            <li>Array Methods</li>
+                            <li>DOM</li>
+                            <li>Element selection</li>
+                            <li>Basic DOM manipulation</li>
+                            <li>Events and event listeners</li>
+                                
+                            </p>
+                            <Button variant="primary" href="https://github.com/AlexisPanyathong/github-usercard" target="_blank">View Here</Button>
+                        </Card.Body>
+                        {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
+                </Card>
+            </CardThree>
+            
+            <CardFour>
+                <Card className="text-center">
+                    <Card.Header>Featured</Card.Header>
+                        <Card.Body>
+                            <HeaderTwo>Form Management</HeaderTwo>
+                            <p>
+                                This project demonstrates proficiency by creating an application that has a registration page and let's you register a new user. Once a user is registered, you can fetch data and render it to the DOM.<br/>
+                                This project consists of:
+                                <li>A registration form built with formik - include inputs for a username and a password.</li>
+                                <li>Form validation using Yup</li>
+                                <li>At least one class component (I would challenge you to make the form a class!)</li>
+                                <li>Tests for at least one element, one event, and one unit test for a function.</li>
+                                <li>A POST request made from the registration form to http://localhost:5000/api/register to add a user. </li>
+                                <li>A GET request made to http://localhost:5000/api/restricted/data to retrieve the data. </li>
+                                <li>Map over the array of objects you recieve and render some or all of it's information to the DOM.</li>		
+                            </p>
+                            <Button variant="primary" href="https://github.com/AlexisPanyathong/github-usercard" target="_blank">View Here</Button>
+                        </Card.Body>
+                        {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
+                </Card>
+            </CardFour>
+
+            <CardFive>
+                <Card className="text-center">
+                    <Card.Header>Featured</Card.Header>
+                        <Card.Body>
+                            <HeaderTwo>Java SnackBar</HeaderTwo>
+                            <p>
+                            The main object of this project is to set up vending machines where a person buys snacks from those machines.<br/>
+                                This project consists of:
+                                <li>Use the Java Development Kit (JDK)</li>
+                                <li>Use and manipulate Java base data types</li>
+                                <li>Construct Java classes including instantiating and using Java Objects</li>
+                                <li>Use and manipulate Java base data types	</li>
+                            </p>
+                            <Button variant="primary" href="https://github.com/AlexisPanyathong/java-SnackBar/tree/alexis-panyathong-2" target="_blank">View Here</Button>
+                        </Card.Body>
+                        {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
+                </Card>
+            </CardFive>
+
+            <CardSix>
+                <Card className="text-center">
+                    <Card.Header>Featured</Card.Header>
+                        <Card.Body>
+                            <HeaderTwo>Dungeon of the Haunted Forest</HeaderTwo>
+                            <p>
+                                I built an interactive Multi-User Dungeon (MUD) client and server with a small group. I chose to work in the Front End side by creating the Registration, Login, and NavBar components that connects to the back end that we used. I also did some UI design for the Welcome Page and NavBar. I used React and JavaScript throughout the project.
+                            </p>
+                            <Button variant="primary" href="https://github.com/build-week-mud/MUD-FE" target="_blank">View Here</Button>
+                        </Card.Body>
+                        {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
+                </Card>
+            </CardSix>
+            
             <footer className="social-icons">
                 <Icons>
                     <SocialIcon url="https://twitter.com/Alex_Panyathong" style={{ height: 40, width: 70 }} target="_blank"/>    
